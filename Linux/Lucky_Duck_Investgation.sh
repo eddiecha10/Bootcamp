16c541de2771f9c8eec0c35fe3b3f987bfea6f75
sysadmin <eddiecha10@yahoo.com>
2021-09-26 22:38:18 -0400

    First Commit

---
+++ Linux/Lucky_Duck_Investgation.sh
@@ -0,0 +1,3 @@
+awk -F" " '{print $1, $2, $5, $6}' 0310_Dealer_schedule | grep "Billy Jones"
+awk -F" " '{print $1, $2, $5, $6}' 0312_Dealer_schedule | grep "Billy Jones"
+awk -F" " '{print $1, $2, $5, $6}' 0315_Dealer_schedule | grep "Billy Jones"
